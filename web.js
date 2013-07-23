f2759e75bdb17a4437efa22acabadb4d187eb7c6
Nick Graczyk <nick.graczyk@gmail.com>
2013-07-23 04:59:50 +0000

    updated with bootstrap

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var file = fs.readFileSync('index.html');
+	var file = fs.readFileSync(htmlfile).toString();
 	response.send(file.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
